ad6d67ab54ee9cd04f3df37d1ba82e307f76d140
saqibishfaque002 <saqibishfaquekhi@gmail.com>
2023-08-02 15:42:08 +0500

    Mini CMS

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome Mini CMS!</title>
+    <meta name="description" content="The small framework with powerful features">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<!-- HEADER: MENU + HEROE SECTION -->
+<header>
+    <div class="heroe">
+
+        <h1>Welcome to Mini CMS</h1>
+    </div>
+
+</header>
+
+<!-- CONTENT -->
+
+<section>
+
+    <h1>Hello World</h1>
+
+</section>
+
+</body>
+</html>
